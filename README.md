b18d84062cd96659b469192ad9605f84cb31b317
marciomelo@gmail.com <marciomelo@gmail.com>
2018-01-19 21:16:26 -0300

    Add the viewer for the instagram photos

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ A Viewer for Instagram Photos usingo NodeJS
 - Instagram Node package (https://www.npmjs.com/package/instagram-node)
 - EJS (https://www.embeddedjs.com)
 - EJS Package (https://www.npmjs.com/package/ejs)
+
+### Needs:
+- Instagram Access Token: grab at http://instagram.pixelunion.net
+
+## Run
+- node install
+- node server.js or nodemon server.js
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var ig = require('instagram-node');
+var ig = require('instagram-node').instagram();
 
 var app = express();
 
@@ -9,11 +9,22 @@ app.use(express.static(__dirname + '/public'));
 //set the view engine ejs
 app.set('view engine', 'ejs');
 
-//instagram access token
 
-//routes
+
+//instagram access token (grab at )
+ig.use({
+	access_token: 'MY ACCESS TOKEN', 
+});
+
+//routes - home -> popular images
 app.get('/', function(req, res) {
-	res.render('pages/index');
+	//use the instagram package to get popular media
+	 ig.user_self_media_recent(function(err, medias, pagination, remaining, limit) {        
+	 	// render the home page and pass in the popular images       
+		res.render('pages/index', { grams: medias });    
+	});
+
+	
 });
 
 //start the server
